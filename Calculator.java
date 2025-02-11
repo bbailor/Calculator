0684b225f544133471956e97e75ae7550c3e828f
bbailor <bensamb06@gmail.com>
2025-02-11 09:09:13 -0500

    Added the additional option to find the difference between two integers.

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,7 @@ public class Calculator
 {
     public static void main(String[] args)
     {
-        System.out.println("\nEnter two numbers to be added together.");
+        System.out.println("\nEnter two numbers to add or subtract.");
         
         //Creates a new instance of the scanner class.
         Scanner input = new Scanner(System.in);
@@ -17,9 +17,23 @@ public class Calculator
         System.out.print("\nEnter the second integer: ");
         int int2 = input.nextInt();
 
-        //Adds the integers together and prints the sum.
-        System.out.println("\n\nThe sum is " + (int1 + int2) + ".");
+        //Prompts the user to enter an operation. Stores the value as an integer that corresponds to an operation.
+        System.out.print("\nEnter \"1\" to add the integers, or enter \"2\" to subtract: ");
+        int operation = input.nextInt();
 
+        //Switch statement to perform the correct operation.
+        //Default block accounts for invalid entry.
+        switch (operation)
+        {
+            case 1:
+                System.out.println("\n\nThe sum is " + (int1 + int2) + ".");
+                break;
+            case 2:
+                System.out.println("\n\nThe difference is " + (int1 - int2) + ".");
+                break;
+            default:
+                System.out.println("\n\nAn invalid operation was entered.");
+        }
 
         input.close();
     }
