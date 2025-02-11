ff90ac199c096c498c562c32cb0c319ea7fafb7c
bbailor <bensamb06@gmail.com>
2025-02-11 08:58:35 -0500

    Added comments for readability.

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,4 @@
+//Imports the scanner class.
 import java.util.Scanner;
 
 public class Calculator
@@ -6,15 +7,20 @@ public class Calculator
     {
         System.out.println("\nEnter two numbers to be added together.");
         
+        //Creates a new instance of the scanner class.
         Scanner input = new Scanner(System.in);
 
+        //Prompts the user to enter two integers and stores them as int values. Uses the nextInt() method.
         System.out.print("\n\nEnter the first integer: ");
         int int1 = input.nextInt();
 
         System.out.print("\nEnter the second integer: ");
         int int2 = input.nextInt();
 
+        //Adds the integers together and prints the sum.
         System.out.println("\n\nThe sum is " + (int1 + int2) + ".");
+
+
         input.close();
     }
 }
